e823ed1886155013ce080dca8c0a220fbdf433aa
Sergey Kabanets <skabanets97@gmail.com>
2025-03-14 14:16:36 +0200

    Add ChatButton component

---
+++ src/ChatButton/ChatButton.tsx
@@ -0,0 +1,48 @@
+import React, { FC } from "react";
+import "./ChatButton.css";
+
+interface ChatButtonProps {
+  label: string;
+  onClick: () => void;
+  disabled?: boolean;
+  icon?: React.ReactNode;
+  className?: string;
+  variant?: "primary" | "secondary" | "danger";
+}
+
+const ChatButton: FC<ChatButtonProps> = ({
+  label,
+  onClick,
+  disabled = false,
+  icon,
+  className = "",
+  variant = "primary",
+}) => {
+  let buttonClass = "";
+  switch (variant) {
+    case "secondary":
+      buttonClass = "buttonSecondary";
+      break;
+    case "danger":
+      buttonClass = "buttonDanger";
+      break;
+    case "primary":
+    default:
+      buttonClass = "buttonPrimary";
+      break;
+  }
+
+  return (
+    <button
+      className={`button ${buttonClass} ${className} ${
+        disabled ? "buttonDisabled" : ""
+      }`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {icon && <span>{icon}</span>} {label}
+    </button>
+  );
+};
+
+export default ChatButton;
